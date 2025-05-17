9c1b4ad40c4823648b25ec031007250821a9380a
lgonzalez876 <lucigon@stanford.edu>
2025-05-17 14:03:59 -0700

    box

--- scripts/box_front.gd
+++ scripts/box_front.gd
@@ -2,11 +2,11 @@ extends Sprite2D
 
 @onready var area = $Area2D
 @export var fade_frames: int = 10
+@export var keep_hidden: bool = false
 
 var bodies_in_area = 0
-var fade_progress = 0.0
+var current_alpha = 0.0
 var target_alpha = 1.0
-var current_alpha = 1.0
 
 func _ready():
 	area.body_entered.connect(_on_body_entered)
@@ -14,23 +14,22 @@ func _ready():
 	modulate.a = current_alpha
 
 func _process(delta):
-	if fade_progress < 1.0:
-		fade_progress += 1.0 / fade_frames
-		current_alpha = lerp(current_alpha, target_alpha, fade_progress)
-		modulate.a = current_alpha
-		if fade_progress >= 1.0:
-			fade_progress = 0.0
+	var alpha_shift = 1.0 / fade_frames
+	if bodies_in_area > 0 or keep_hidden:
+		target_alpha = 0.0
+	else:
+		target_alpha = 1.0
+	
+	if target_alpha > current_alpha:
+		current_alpha += alpha_shift
+	elif target_alpha < current_alpha:
+		current_alpha -= alpha_shift
+	modulate.a = current_alpha
 
 func _on_body_entered(body):
+	print("_on_body_entered")
 	bodies_in_area += 1
-	print("body entered box area")
-	if bodies_in_area == 1:  # Only start fade if this is the first body
-		target_alpha = 0.0
-		fade_progress = 0.0
 
 func _on_body_exited(body):
-	print("body exited box area")
+	print("_on_body_exited")
 	bodies_in_area -= 1
-	if bodies_in_area == 0:  # Only start fade if this was the last body
-		target_alpha = 1.0
-		fade_progress = 0.0
