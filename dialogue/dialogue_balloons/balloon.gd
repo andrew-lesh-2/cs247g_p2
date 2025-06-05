0994537a6bfbd86a5fe902df527b6e821801f274
Destiny TSM <destinyt@stanford.edu>
2025-06-05 14:27:49 -0700

    Dialogue Box Rendering
    Changed names of portrait files to simplify code, code now calls individual portraits dependent on sprites, customized dialogue box for placement of portraits, text, etc.

--- dialogue/dialogue_balloons/balloon.gd
+++ dialogue/dialogue_balloons/balloon.gd
@@ -41,7 +41,8 @@ var mutation_cooldown: Timer = Timer.new()
 
 ## The base balloon anchor
 @onready var balloon: Control = %Balloon
-
+## The character portrait
+@onready var portrait: TextureRect = %Portrait
 ## The label showing the name of the currently speaking character
 @onready var character_label: RichTextLabel = %CharacterLabel
 
@@ -97,6 +98,11 @@ func apply_dialogue_line() -> void:
 
 	character_label.visible = not dialogue_line.character.is_empty()
 	character_label.text = tr(dialogue_line.character, "dialogue")
+	var portrait_path: String = "res://dialogue/dialogue_assets/%s.png" % dialogue_line.character
+	if ResourceLoader.exists(portrait_path):
+		portrait.texture = load(portrait_path)
+	else:
+		portrait.texture = null
 
 	dialogue_label.hide()
 	dialogue_label.dialogue_line = dialogue_line
